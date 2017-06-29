4fe3847043006f75eece7ec1c4cb1ce2da08ff9c
stephengunter <traders.com.tw@gmail.com>
2017-06-29 08:07:53 +0800

    20170629_1

--- app/Listeners/StudentEventListener.php
+++ app/Listeners/StudentEventListener.php
@@ -11,11 +11,21 @@ class StudentEventListener
         $signup=$event->signup;
         $student=Student::where('user_id',$signup->user_id)
                           ->where('course_id',$signup->course_id)
-                          ->get();
+                          ->first();
+
+        
+                          
         if($student){
-            $student->active=false;
-            $student->out_date=$signup->refund->date;
-            $student->save();
+            if($signup->hasRefund()){
+                $student->active=false;
+                $student->out_date=$signup->refund->date;
+                $student->save();
+            }else{
+                $student->active=true;
+                $student->out_date=null;
+                $student->save();
+            }
+            
         }
 
 
