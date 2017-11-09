b86843cbe3832bb8d0360857de5eb3645070eaa2
stephengunter <traders.com.tw@gmail.com>
2017-11-09 09:26:32 +0800

    20171109-15

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -45,6 +45,37 @@ class AdminSeeder extends Seeder
         $admin=Admin::find($user->id);
         $admin->attachCenter($center->id);
 
+
+
+        $user=\App\User::create([
+			'name' => '花組員',
+			'email' =>'test01@tcust.com.tw',
+			'phone' => '0936000000',
+			'password' => '000000',
+			'email_confirmed' => true,
+			'remember_token' => str_random(10),
+		]);
+		$profile=new \App\Profile([
+			'user_id' => $user->id,					
+			'fullname'=> '花組員',
+			'dob' =>'1970-1-1',
+			'gender' => 1,
+		]);
+        $user->profile()->save($profile); 
+        
+        $roleName=Role::adminRoleName();
+		$user->addRole($roleName);
+
+        $adminValues = [
+		     'role' => $roleName,	
+             'user_id' => $user->id,
+		 ];
+        Admin::create($adminValues);
+
+        $center=Center::where('head',true)->first();
+        $admin=Admin::find($user->id);
+        $admin->attachCenter($center->id);
+
         
         
 		
